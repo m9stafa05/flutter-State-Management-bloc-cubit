ff543f31e470f3246c731253a1e3d6612de74961
m9stafa05 <m9stafa05@gmail.com>
2025-06-26 17:04:10 +0300

    Refactor TaskCubit: use String for task IDs

--- lib/controller/cubit/task_cubit.dart
+++ lib/controller/cubit/task_cubit.dart
@@ -2,25 +2,31 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/models/task_model.dart';
+import 'package:uuid/uuid.dart';
 
 part 'task_state.dart';
 
 class TaskCubit extends Cubit<TaskState> {
   TaskCubit() : super(InitialTask());
 
-  void addTask(TaskModel model) {
+  void addTask({required String title}) {
+    final model = TaskModel(
+      title: title,
+      isCompleted: false,
+      id: Uuid().v1(),
+    );
     // emit(UpdateTask(List.from(state.tasks)..add(model)));
     emit(UpdateTask([...state.tasksList, model]));
   }
 
-  void removeTask(int id) {
+  void removeTask(String id) {
     final List<TaskModel> newList = state.tasksList.where((task) {
       return task.id != id;
     }).toList();
     emit(UpdateTask(newList));
   }
 
-  void toggleTask(int id) {
+  void toggleTask(String id) {
     final List<TaskModel> newList = state.tasksList.map((task) {
       if (task.id == id) {
         return task.copyWith(isCompleted: !task.isCompleted);
