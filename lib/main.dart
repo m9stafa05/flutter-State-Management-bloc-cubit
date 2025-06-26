601a0c3c1c72fd399c26f708a6fc556179d898fc
m9stafa05 <m9stafa05@gmail.com>
2025-06-26 17:03:53 +0300

    Implement task management features with Bloc: add task input, list, and actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_management/controller/cubit/task_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,28 +20,96 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
-
+  final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Text('Hello, Flutter!'),
+      appBar: AppBar(title: Text(title)),
+      body: BlocProvider(
+        create: (context) => TaskCubit(),
+        child: BlocBuilder<TaskCubit, TaskState>(
+          builder: (context, state) {
+            final cubitController = context.read<TaskCubit>();
+            return Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                    ),
+                    child: TextField(
+                      controller: controller,
+                      decoration: InputDecoration(
+                        hintText: 'Add Task',
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (controller.text.isEmpty) return;
+                      cubitController.addTask(title: controller.text);
+                      controller.clear();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text('Add'),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: state.tasksList.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            state.tasksList[index].title ?? "",
+                          ),
+                          leading: Checkbox(
+                            value: state.tasksList[index].isCompleted,
+                            onChanged: (v) {
+                              cubitController.toggleTask(
+                                state.tasksList[index].id,
+                              );
+                            },
+                          ),
+                          trailing: IconButton(
+                            onPressed: () {
+                              cubitController.removeTask(
+                                state.tasksList[index].id,
+                              );
+                            },
+                            icon: Icon(Icons.delete),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 }
-
