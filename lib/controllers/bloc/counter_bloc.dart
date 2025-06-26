946c81774f10ad4d7f032e8d4304fd4c64fb4594
m9stafa05 <m9stafa05@gmail.com>
2025-06-26 14:03:18 +0300

    Add CounterBloc, CounterEvent, and CounterState classes for state management

---
+++ lib/controllers/bloc/counter_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterInit()) {
+    on<CounterIncrement>((event, emit) {
+      emit(CounterUpdate(state.count + 1));
+    });
+    on<CounterDecrement>((event, emit) {
+      emit(CounterUpdate(state.count - 1));
+    });
+  }
+}
