0ec8b9ff86db9376d5c24dbd49f74d20bea53473
m9stafa05 <m9stafa05@gmail.com>
2025-06-26 15:04:34 +0300

    Use The Equatable In Bloc

--- lib/controllers/bloc/counter_state.dart
+++ lib/controllers/bloc/counter_state.dart
@@ -1,10 +1,12 @@
 part of 'counter_bloc.dart';
 
-
-sealed class CounterState {
+sealed class CounterState extends Equatable {
   final int count;
 
   const CounterState({required this.count});
+
+  @override
+  List<Object?> get props => [count];
 }
 
 class CounterInit extends CounterState {
