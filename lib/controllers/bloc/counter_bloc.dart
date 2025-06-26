34027410f8e8886d07ddd25618f327c96a23ec5d
m9stafa05 <m9stafa05@gmail.com>
2025-06-26 12:32:31 +0300

    Implement CounterBloc with Increment and Decrement events, and define CounterState classes

---
+++ lib/controllers/bloc/counter_bloc.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterInit()) {
+    on<IncrementEvent>((event, emit) {
+      emit(CounterUpdate(state.count + 1));
+    });
+
+    on<DecrementEvent>((event, emit) {
+      if (state.count == 0) return;
+      emit(CounterUpdate(state.count - 1));
+    });
+  }
+}
