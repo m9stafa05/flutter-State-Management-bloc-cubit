16d63e81e500b211ffbe8aa048813a25edd41a12
m9stafa05 <m9stafa05@gmail.com>
2025-06-26 14:24:53 +0300

    Initlaize CounterState

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -3,15 +3,15 @@ import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterInit());
 
   void increment() {
-    emit(state + 1);
+    emit(CounterUpdate(state.count + 1));
   }
 
   void decrement() {
-    if (state == 0) return;
-    emit(state - 1);
+    if (state.count == 0) return;
+    emit(CounterUpdate(state.count - 1));
   }
 }
