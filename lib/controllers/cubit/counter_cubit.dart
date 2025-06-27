8710734fa8193314a0d1cb98834d4c2bb39e3d6a
m9stafa05 <m9stafa05@gmail.com>
2025-06-27 15:49:03 +0300

    Refactor CounterCubit to extend HydratedCubit for state persistence

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -1,10 +1,10 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
+class CounterCubit extends HydratedCubit<CounterState> {
   CounterCubit() : super(CounterInit());
 
   void increment() {
@@ -14,5 +14,15 @@ class CounterCubit extends Cubit<CounterState> {
   void decrement() {
     if (state.count == 0) return;
     emit(CounterUpdate(state.count - 1));
+  }
+
+  @override
+  CounterState? fromJson(Map<String, dynamic> json) {
+    return CounterUpdate(json['count']);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(CounterState state) {
+    return {'count': state.count};
   }
 }
