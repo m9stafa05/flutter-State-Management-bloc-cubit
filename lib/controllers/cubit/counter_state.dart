935d5b285bef33b5a4365db584345723a715ae10
m9stafa05 <m9stafa05@gmail.com>
2025-07-01 13:57:53 +0300

    Refactor CounterState to remove sealed modifier and add copyWith method

--- lib/controllers/cubit/counter_state.dart
+++ lib/controllers/cubit/counter_state.dart
@@ -1,10 +1,14 @@
 part of 'counter_cubit.dart';
 
 @immutable
-sealed class CounterState extends Equatable {
+class CounterState extends Equatable {
   final int count;
 
   const CounterState({required this.count});
+
+  CounterState copyWith({int? count}) {
+    return CounterState(count: count ?? this.count);
+  }
 
   @override
   List<Object?> get props => [count];
